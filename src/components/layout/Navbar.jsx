ca888f98f0dfaaeb601130ffc37b1a983c766767
Agustin Urien <agus.urien2@gmail.com>
2025-03-06 20:22:49 -0300

    navbar finished

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,24 +1,46 @@
+
 import './navbar.css';
+import { useState } from "react";
+import logo from '../../../public/images/logo.png'
 
 const Navbar = () => {
+    const [linkSelected, setLinkSelected] = useState('home');
+
+
+    const links = [
+        { id: 'home', label: 'Home', href: '#' },
+        { id: 'quienes', label: 'Quienes somos', href: '#about' },
+        { id: 'equipo', label: 'Equipo', href: '#services' },
+        { id: 'propiedades', label: 'Propiedades', href: '#contact' },
+        { id: 'contacto', label: 'Contacto', href: '#contact' }
+    ];
+
     return (
         <nav className='navbar'>
-            <div>
-                <h1>Logo</h1>
+            <div className='logoContainer'>
+                <img
+                    src={logo.src}
+                    height={50}
+                    width={50}
+                    alt="Logo de la empresa"
+                />
             </div>
 
             <div className='navLinksContainer'>
                 <ul className='navLinks'>
-                    <li><a href=''>Home</a></li>
-                    <li><a href='#about'>Quienes somos</a></li>
-                    <li><a href='#services'>Equipo</a></li>
-                    <li><a href='#contact'>Propiedades</a></li>
-                    <li><a href='#contact'>Contacto</a></li>
+                    {links.map(link => (
+                        <li key={link.id}>
+                            {linkSelected === link.id &&
+                                <div className='imgContainer'>
+                                </div>
+                            }
+                            <a href={link.href} onClick={() => setLinkSelected(link.id)} style={{ color: linkSelected === link.id && 'white' }}>{link.label}</a>
+                        </li>
+                    ))}
                 </ul>
             </div>
-
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
