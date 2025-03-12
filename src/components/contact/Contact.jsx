12352981ca055b1747b50d38a39c185bd6d91d31
MacBookPro <macbookpro@MacBook-Pro-de-MacBookPro.local>
2025-03-12 12:05:45 -0300

    termino el footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,49 +1,72 @@
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import "../contact/contact.css";
 const Contact = () => {
-    const handlesubmit = (e) => {
-        e.preventDefault();
+  const handlesubmit = (e) => {
+    e.preventDefault();
 
-        const form = e.target;
+    const form = e.target;
 
-        const templateParams = {
-            from_name: form.name.value,
-            number: form.number.value,
-            from_email: form.email.value,
-            message: form.message.value
-        };
-        
-        const serviceID = 'service_8orziyc';
-        const templateID = 'template_kwzkqs8';
-        const publicKey = '9kc7Ztm86wijQM7vx';
+    const templateParams = {
+      from_name: form.name.value,
+      number: form.number.value,
+      from_email: form.email.value,
+      message: form.message.value,
+    };
 
-        emailjs.send(serviceID, templateID, templateParams, publicKey)
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+    const serviceID = "service_8orziyc";
+    const templateID = "template_kwzkqs8";
+    const publicKey = "9kc7Ztm86wijQM7vx";
 
-        form.reset();
-    }
+    emailjs.send(serviceID, templateID, templateParams, publicKey).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+
+    form.reset();
+  };
   return (
-    <section className='contactContainer'>
-
-    <div className='contact'>
+    <section className="contactContainer" id="contacto">
+      <div className="contact">
         <h2>Contacto</h2>
         <form onSubmit={handlesubmit}>
-
-            <input type="text" id='name' name='name' placeholder='Nombre' required />
-            <input type="number" id='number' name='number' placeholder='Celular' required />
-            <input type="email" id='email' name='email' placeholder='Email' required />
-            <textarea type="text" id='message' name='message' placeholder='Mensaje' className='area' required />
-            <button>Enviar</button>
-
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Nombre"
+            required
+          />
+          <input
+            type="number"
+            id="number"
+            name="number"
+            placeholder="Celular"
+            required
+          />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email"
+            required
+          />
+          <textarea
+            type="text"
+            id="message"
+            name="message"
+            placeholder="Mensaje"
+            className="area"
+            required
+          />
+          <button>Enviar</button>
         </form>
-    </div>
+      </div>
     </section>
+  );
+};
 
-  )
-}
-
-export default Contact
+export default Contact;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,46 +1,46 @@
-
-import './navbar.css';
+import "./navbar.css";
 import { useState } from "react";
-import logo from '../../../public/images/logo.jpg'
+import logo from "../../../public/images/logo.jpg";
 
 const Navbar = () => {
-    const [linkSelected, setLinkSelected] = useState('home');
+  const [linkSelected, setLinkSelected] = useState("home");
 
+  const links = [
+    { id: "home", label: "Home", href: "/" },
+    { id: "quienes", label: "Quienes somos", href: "#quienes" },
+    { id: "equipo", label: "Equipo", href: "#equipo" },
+    {
+      id: "propiedades",
+      label: "Propiedades",
+      href: "https://www.globalreddeinmobiliarias.com/",
+    },
+    { id: "contacto", label: "Contacto", href: "#contacto" },
+  ];
 
-    const links = [
-        { id: 'home', label: 'Home', href: '#' },
-        { id: 'quienes', label: 'Quienes somos', href: '#about' },
-        { id: 'equipo', label: 'Equipo', href: '#services' },
-        { id: 'propiedades', label: 'Propiedades', href: '#contact' },
-        { id: 'contacto', label: 'Contacto', href: '#contact' }
-    ];
+  return (
+    <nav className="navbar">
+      <div className="logoContainer">
+        <img src={logo.src} height={50} width={50} alt="Logo de la empresa" />
+      </div>
 
-    return (
-        <nav className='navbar'>
-            <div className='logoContainer'>
-                <img
-                    src={logo.src}
-                    height={50}
-                    width={50}
-                    alt="Logo de la empresa"
-                />
-            </div>
-
-            <div className='navLinksContainer'>
-                <ul className='navLinks'>
-                    {links.map(link => (
-                        <li key={link.id}>
-                            {linkSelected === link.id &&
-                                <div className='imgContainer'>
-                                </div>
-                            }
-                            <a href={link.href} onClick={() => setLinkSelected(link.id)} style={{ color: linkSelected === link.id && 'white' }}>{link.label}</a>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    )
-}
+      <div className="navLinksContainer">
+        <ul className="navLinks">
+          {links.map((link) => (
+            <li key={link.id}>
+              {linkSelected === link.id && <div className="imgContainer"></div>}
+              <a
+                href={link.href}
+                onClick={() => setLinkSelected(link.id)}
+                style={{ color: linkSelected === link.id && "white" }}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
